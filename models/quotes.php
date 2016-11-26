88dd4a90410b2310202497f6eb682a35743fefb6
mm <alasmichellemarie@gmail.com>
2016-11-26 14:23:27 +0800

    id

--- models/quotes.php
+++ models/quotes.php
@@ -21,14 +21,17 @@ class Quotes {
 	{
 		return array(
 			'101' => [
+				'id' => 101,
 				'text' => "Never give up on a dream just because of the time it will take to accomplish it. The time will pass anyway",
 				'image_url' => "http://addicted2success.com/wp-content/uploads/2013/12/accomplish-dreams-picture-quote.jpg"
 			],
 			'102' => [
+				'id' => 102,
 				'text' => "What you think of yourself is much more important than what people think of you",
 				'image_url' => "http://addicted2success.com/wp-content/uploads/2013/12/picture-quote-think-of-yourself.jpg"
 			],
 			'103' => [
+				'id' => 103,
 				'text' => "Its funny how the people that hurt you the most are the ones that swore they never would",
 				'image_url' => "http://picpulp.com/wp-content/uploads/2013/03/trust-quotes-14.jpg"
 			]
